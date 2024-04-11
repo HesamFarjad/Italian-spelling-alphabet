941b1a004526f1aa2f47ab578da05e851b8ce4b8
Hesam Farjad <81914229+HesamFarjad@users.noreply.github.com>
2024-04-11 14:01:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,17 @@
-from turtle import Turtle, Screen
 import pandas
-
-# image =""
-# t = Turtle()
-# t.penup()
-# t.hideturtle()
-# screen = Screen()
-# screen.addshape()
-# t.shape()
 
 # Comprehension List + Comprehension Dictionary
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 new_dic = {row.letter: row.code for (index, row) in data.iterrows()}
-# print(new_dic)
-name_inserted = input("Insert a word :").upper()
-final_alph_name = [new_dic[i] for i in name_inserted]
-print(final_alph_name)
 
+go_on = True
 
-
-# screen.exitonclick()
+while go_on:
+    try:
+        name_inserted = input("Insert a word :").upper()
+        final_alph_name = [new_dic[i] for i in name_inserted]
+    except KeyError:
+        print("Please enter a valid value (Character).")
+    else:
+        print(final_alph_name)
+        go_on = False
